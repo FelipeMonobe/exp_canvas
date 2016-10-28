0e7d29d32fed1b9668a142112345b192ba4d3f1d
FelipeMonobe <xinube@gmail.com>
2016-10-28 20:13:58 -0200

    img.onload

--- main.js
+++ main.js
@@ -8,11 +8,13 @@ fs.readFile(__dirname + '/image.jpg', (err, file) => {
   let img = new Image;
   img.src = file;
 
-  let canvas = new Canvas(img.width, img.height, 'pdf');
-  let ctx = canvas.getContext('2d');
+  img.onload = () => {
+    let canvas = new Canvas(img.width, img.height, 'pdf');
+    let ctx = canvas.getContext('2d');
 
-  ctx.drawImage(img, 0, 0, img.width, img.height);
-  ctx.addPage();
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    ctx.addPage();
 
-  fs.writeFile('out.pdf', canvas.toBuffer());
+    fs.writeFile('out.pdf', canvas.toBuffer());
+  };
 });
